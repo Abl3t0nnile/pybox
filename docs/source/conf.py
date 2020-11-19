31226bb3488602b2d30066a1ac34d755110e4146
Abl3t0nnile <abletonnile@googlemail.com>
2020-11-19 21:54:25 +0100

    edit sys.path in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ import sys
 
 from sphinx_typlog_theme import add_badge_roles
 
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
